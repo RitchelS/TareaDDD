d3912a2d868e84042021b473a9c346af4b8cbae0
DiegoSalazar20 <dsalazarzamora2001@gmail.com>
2025-04-28 16:48:40 -0600

    Contexto de la base de datos, entidad de persona y clase de persona repositorio

---
+++ Infraestuctura/Contexto/ContextoDB.cs
@@ -0,0 +1,18 @@
+﻿using Infraestuctura.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestuctura.Contexto
+{
+    public class ContextoDB : DbContext
+    {
+        public ContextoDB(DbContextOptions<ContextoDB> options) : base(options)
+        {
+        }
+        public DbSet<PersonaEntity> Personas { get; set; }
+    }
+}
---
+++ Infraestuctura/Entidades/PersonaEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestuctura.Entidades
+{
+    [Table("persona")]
+    public class PersonaEntity
+    {
+        [Key]
+        public int IdPersona { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido1 { get; set; }
+        [Required]
+        public string Apellido2 { get; set; }
+        [Required]
+        public string Correo { get; set; }
+    }
+}
