aaa8f12f7597da3129c3b05cd005afcca9fa2747
RitchelS <sojoritchel@gmail.com>
2025-04-28 16:19:28 -0600

    Entidades y repositorio en proyecto Dominio

---
+++ Dominio/Repositorios/IPersonaRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio.Entidades;
+
+namespace Dominio.Repositorios
+{
+    public interface IPersonaRepositorio
+    {
+        public Task<bool> ActualizarDatosPersona(int idPersona,string nombre, string apellido1, string apellido2, string correo);
+        public Task<Persona> BuscarPersonaPorId(int idPersona);
+        public Task<IEnumerable<Persona>> ObtenerPersonas();
+        public Task<bool> RegistrarPersona(Persona persona);
+        public Task<Persona> EliminarPersona(int idPersona);
+    }
+}
--- Infraestuctura/obj/Debug/net8.0/Infraestuctura.AssemblyInfo.cs
+++ Infraestuctura/obj/Debug/net8.0/Infraestuctura.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestuctura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc70387c49aa77a4f2e35537985918cc71978ab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestuctura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestuctura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
