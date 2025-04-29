b437ee82f9992be54d699ac785ecd902d50a7504
DiegoSalazar20 <dsalazarzamora2001@gmail.com>
2025-04-29 10:11:05 -0600

    Correccion en mappers de persona

---
+++ Presentacion.API/Mappers/PersonaMapper.cs
@@ -0,0 +1,30 @@
+﻿using Dominio.Entidades;
+using Presentacion.API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion.Mappers
+{
+    public static class PersonaMapper
+    {
+        public static Persona ConvertirDtoAPersona(PersonaDTO dto)
+        {
+            return new Persona(dto.Nombre, dto.Apellido1, dto.Apellido2, dto.Correo);
+        }
+
+        public static PersonaRespuestaDTO ConvertirPersonaADto(Persona persona)
+        {
+            return new PersonaRespuestaDTO
+            {
+                IdPersona = persona.IdPersona,
+                Nombre = persona.Nombre,
+                Apellido1 = persona.Apellido1,
+                Apellido2 = persona.Apellido2,
+                Correo = persona.Correo
+            };
+        }
+    }
+}
